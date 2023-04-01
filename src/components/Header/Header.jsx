a5ef271fbd7750ca6da4f686bb73c4b2b454b0db
NushratNowrin <nowrin2023@gmail.com>
2023-04-01 23:13:14 +0600

    Header responsive done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,10 +12,18 @@ const Header = () => {
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/about">About</a></li>
             </div>
+
             <div className='image'>
                 <img src={image} alt="" />
             </div>
         </div>
+        <div className='mobile-navbar'>
+                
+                <li><a href="/home">Home</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/about">About</a></li>
+                
+            </div>
         <hr />
         </div>
 
