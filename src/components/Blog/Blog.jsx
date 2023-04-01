70db9088335071762364aa4c5428e1f6b03cc9a6
NushratNowrin <nowrin2023@gmail.com>
2023-04-01 11:42:24 +0600

    Blogs UI done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Blog.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Blog = (props) => {
 	const { id, name, date, time, title, img, authorImg } = props.blog;
@@ -7,15 +9,25 @@ const Blog = (props) => {
 		<div className='blog'>
 			<img src={img} alt='Study' className='blog-img' />
 			<div className='blog-special-info'>
-				<div className="blog-special-info-right">
+				<div className='blog-special-info-left'>
 					<img src={authorImg} alt='Author' />
 					<div>
 						<h4>{name}</h4>
 						<small className='smaller-text'>{date}</small>
 					</div>
 				</div>
-				<p className='read-time'>{time} min read</p>
+				<div className="blog-special-info-right">
+					<p className='read-time'>{time} min read</p>
+					<button className="different-btn">
+						<FontAwesomeIcon icon={faBookmark} />
+					</button>
+				</div>
 			</div>
+            <h2 className="blog-title">{title}</h2>
+            <small className="smaller-text">#beginners &nbsp; #programming</small><br /><br />
+            <button className="different-btn read-btn">
+                <a href="">Mark as read</a>
+            </button>
 		</div>
 	);
 };
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <div>
+                <h4>Spent time on read : </h4>
+            </div>
+            <div>
+                <h4>Bookmark Blogs : </h4>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
